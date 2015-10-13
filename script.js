86f72b598ad0e31340fe7e0d5e78ba05ed0715d0
Alessandra Cannatà <pan.kairos@gmail.com>
2015-10-13 11:58:27 +0200

    mouseenter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Exercise 2: Color picker */
-var colors = [ "22ac5e", "d68236", "99ca2d"];
+var colors = [ "22ac5e", "d68236", "99ca2d", "ff6961", "CB99C9"];
 
 function setPreviewColor(color) {
   $(".preview").css("background-color", color);
@@ -11,16 +11,26 @@ function addBox(color) {
 }
 
 $(document).ready(function() {
-  setPreviewColor("red");
+  random_position = Math.floor( Math.random() * colors.length );
+  setPreviewColor(colors[random_position]);
+
   $.each(colors, function(index, element) {
     addBox(element);
   });
+
   $(document).on("keydown keyup keypress", "#color", function() {
     setPreviewColor($(this).val());
   });
 
   $(document).on("click", "#add-to-favorite", function() {
+    if ( $("#colors .item").length >=5 )
+      $("#colors .item").last().remove();
     addBox($("#color").val());
+    $("#color").val("").focus();
+  });
+
+  $(document).on('mouseenter', '.item', function() {
+    setPreviewColor($(this).css('background-color'));
   });
 });
 
